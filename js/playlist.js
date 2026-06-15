66db968718e2c2c91bf19c4cdd2aaae2528002d8
RezFx <rezfx75@gmail.com>
2026-06-15 23:16:04 +0200

    Add files via upload
    sillyphon audioplayer

---
+++ js/playlist.js
@@ -0,0 +1,68 @@
+let songs = [
+  {
+    name: "with all the indifference",
+    artist: "sillyphon",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "universal drive",
+    artist: "sillyphon",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "suspicion",
+    artist: "sillyphon",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "song twister",
+    artist: "sillyphon",
+    img: "img4",
+    audio: "music4"
+  },
+  {
+    name: "sandy c",
+    artist: "sillyphon",
+    img: "img5",
+    audio: "music5"
+  },
+  {
+    name: "maybe",
+    artist: "sillyphon",
+    img: "img6",
+    audio: "music6"
+  },
+  {
+    name: "loopy",
+    artist: "sillyphon",
+    img: "img7",
+    audio: "music7"
+  },
+  {
+    name: "I heard it through the grapevine",
+    artist: "sillyphon",
+    img: "img8",
+    audio: "music8"
+  },
+  {
+    name: "heart of glass",
+    artist: "sillyphon",
+    img: "img9",
+    audio: "music9"
+  },
+  {
+    name: "back to black",
+    artist: "sillyphon",
+    img: "img10",
+    audio: "music10"
+  },
+  {
+    name: "our baby",
+    artist: "sillyphon",
+    img: "img11",
+    audio: "music11"
+  }
+]
